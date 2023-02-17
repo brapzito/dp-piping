e0403dace07c3a08335fcc722d573d627d601696
Zito <brapzito@gmail.com>
2023-02-17 14:05:47 -0700

    content

--- src/pages/Presentation/sections/Counters.js
+++ src/pages/Presentation/sections/Counters.js
@@ -26,32 +26,32 @@ import DefaultCounterCard from "examples/Cards/CounterCards/DefaultCounterCard";
 
 function Counters() {
   return (
-    <MKBox component="section" py={3}>
+    <MKBox component="section" py={3} id="counters">
       <Container>
         <Grid container item xs={12} lg={9} sx={{ mx: "auto" }}>
           <Grid item xs={12} md={4}>
             <DefaultCounterCard
-              count={70}
+              count={100}
               suffix="+"
-              title="Coded Elements"
-              description="From buttons, to inputs, navbars, alerts or cards, you are covered"
+              title="Satisfied customers"
+              description="With over 100 satisfied customers, you can trust your project in our hands"
             />
           </Grid>
           <Grid item xs={12} md={4} display="flex">
             <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, mx: 0 }} />
             <DefaultCounterCard
-              count={15}
+              count={10}
               suffix="+"
-              title="Design Blocks"
-              description="Mix the sections, change the colors and unleash your creativity"
+              title="Years of experience"
+              description="Our experience allows us to deviliver the highest quality in all that we do"
             />
             <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, ml: 0 }} />
           </Grid>
           <Grid item xs={12} md={4}>
             <DefaultCounterCard
-              count={4}
-              title="Pages"
-              description="Save 3-4 weeks of work when you use our pre-made pages for your website"
+              count={365}
+              title="Available 365 days a year"
+              description="24/7 365, we are here to serve you"
             />
           </Grid>
         </Grid>
